04d5b06e7331ec17ec5cede4e604227b8df97926
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-09-08 22:33:13 +0800

    首次修改

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import httpx
 import asyncio
+import os
 from datetime import datetime
 import pandas as pd
 from tqdm.asyncio import tqdm_asyncio
 from tqdm import tqdm
 from common import common
 from typing import Any
+from config import *
 
 url = "https://www.douyin.com/aweme/v1/web/comment/list/"
 reply_url = url + "reply/"
@@ -22,7 +24,8 @@ async def get_comments_async(client: httpx.AsyncClient, aweme_id: str, cursor: s
 
 
 async def fetch_all_comments_async(aweme_id: str) -> list[dict[str, Any]]:
-    async with httpx.AsyncClient() as client:
+    # limits = httpx.Limits(max_connections=1, max_keepalive_connections=1)
+    async with httpx.AsyncClient(timeout=600) as client:
         cursor = 0
         all_comments = []
         has_more = 1
@@ -40,24 +43,24 @@ async def fetch_all_comments_async(aweme_id: str) -> list[dict[str, Any]]:
         return all_comments
 
 
-async def get_replies_async(client: httpx.AsyncClient, comment_id: str, cursor: str = "0", count: str = "50") -> dict[
-    str, Any]:
+async def get_replies_async(client: httpx.AsyncClient, semaphore, comment_id: str, cursor: str = "0",
+                            count: str = "50") -> dict:
     params = {"cursor": cursor, "count": count, "item_type": 0, "item_id": comment_id, "comment_id": comment_id}
     headers = {"cookie": cookie}
     params, headers = common(reply_url, params, headers)
-    response = await client.get(reply_url, params=params, headers=headers)
-    await asyncio.sleep(1)
-    return response.json()
+    async with semaphore:
+        response = await client.get(reply_url, params=params, headers=headers)
+        await asyncio.sleep(1)  # 限制速度，避免请求过快
+        return response.json()
 
 
-async def fetch_replies_for_comment(client: httpx.AsyncClient, comment: dict[str, Any], pbar: tqdm) -> list[
-    dict[str, Any]]:
+async def fetch_replies_for_comment(client: httpx.AsyncClient, semaphore, comment: dict, pbar: tqdm) -> list:
     comment_id = comment["cid"]
     has_more = 1
     cursor = 0
     all_replies = []
     while has_more and comment["reply_comment_total"] > 0:
-        response = await get_replies_async(client, comment_id, cursor=str(cursor))
+        response = await get_replies_async(client, semaphore, comment_id, cursor=str(cursor))
         replies = response.get("comments", [])
         if isinstance(replies, list):
             all_replies.extend(replies)
@@ -69,11 +72,13 @@ async def fetch_replies_for_comment(client: httpx.AsyncClient, comment: dict[str
     return all_replies
 
 
-async def fetch_all_replies_async(comments: list[dict[str, Any]]) -> list[dict[str, Any]]:
+async def fetch_all_replies_async(comments: list) -> list:
     all_replies = []
-    async with httpx.AsyncClient() as client:
+    # limits = httpx.Limits(max_connections=1, max_keepalive_connections=1)
+    async with httpx.AsyncClient(timeout=600) as client:
+        semaphore = asyncio.Semaphore(10)  # 在这里创建信号量
         with tqdm(total=len(comments), desc="Fetching replies", unit="comment") as pbar:
-            tasks = [fetch_replies_for_comment(client, comment, pbar) for comment in comments]
+            tasks = [fetch_replies_for_comment(client, semaphore, comment, pbar) for comment in comments]
             results = await asyncio.gather(*tasks)
             for result in results:
                 all_replies.extend(result)
@@ -84,13 +89,13 @@ def process_comments(comments: list[dict[str, Any]]) -> pd.DataFrame:
     data = [{
         "评论ID": c['cid'],
         "评论内容": c['text'],
-        "点赞数": c['digg_count'],
+        # "点赞数": c['digg_count'],
         "评论时间": datetime.fromtimestamp(c['create_time']).strftime('%Y-%m-%d %H:%M:%S'),
         "用户昵称": c['user']['nickname'],
-        "用户主页链接": f"https://www.douyin.com/user/{c['user']['sec_uid']}",
-        "用户抖音号": c['user']['unique_id'],
-        "用户签名": c['user']['signature'],
-        "回复总数": c['reply_comment_total'],
+        # "用户主页链接": f"https://www.douyin.com/user/{c['user']['sec_uid']}",
+        # "用户抖音号": c['user']['unique_id'],
+        # "用户签名": c['user']['signature'],
+        # "回复总数": c['reply_comment_total'],
     } for c in comments]
     return pd.DataFrame(data)
 
@@ -100,14 +105,14 @@ def process_replies(replies: list[dict[str, Any]], comments: pd.DataFrame) -> pd
         {
             "评论ID": c["cid"],
             "评论内容": c["text"],
-            "点赞数": c["digg_count"],
+            # "点赞数": c["digg_count"],
             "评论时间": datetime.fromtimestamp(c["create_time"]).strftime(
                 "%Y-%m-%d %H:%M:%S"
             ),
             "用户昵称": c["user"]["nickname"],
-            "用户主页链接": f"https://www.douyin.com/user/{c['user']['sec_uid']}",
-            "用户抖音号": c["user"]["unique_id"],
-            "用户签名": c["user"]["signature"],
+            # "用户主页链接": f"https://www.douyin.com/user/{c['user']['sec_uid']}",
+            # "用户抖音号": c["user"]["unique_id"],
+            # "用户签名": c["user"]["signature"],
             "回复的评论": c["reply_id"],
             "具体的回复对象": c["reply_to_reply_id"]
             if c["reply_to_reply_id"] != "0"
@@ -125,23 +130,32 @@ def save(data: pd.DataFrame, filename: str):
     data.to_csv(filename, index=False)
 
 
-aweme_id = input("Enter the aweme_id: ")
-cookie = 'your cookie'
-
-async def main():
+async def process_aweme_id(aweme_id):
+    # 确保 'data' 文件夹存在
+    if not os.path.exists("data"):
+        os.makedirs("data")
     # 评论部分
     all_comments = await fetch_all_comments_async(aweme_id)
-    print(f"Found {len(all_comments)} comments.")
+    print(f"Found {len(all_comments)} comments for aweme_id {aweme_id}.")
+
     all_comments_ = process_comments(all_comments)
-    save(all_comments_, "comments.csv")
+    comments_filename = f"data/{aweme_id}_comments.csv"
+    save(all_comments_, comments_filename)
 
     # 回复部分 如果不需要直接注释掉
     all_replies = await fetch_all_replies_async(all_comments)
-    print(f"Found {len(all_replies)} replies")
-    print(f"Found {len(all_replies) + len(all_comments)} in totals")
+    print(f"Found {len(all_replies)} replies for aweme_id {aweme_id}.")
+    print(f"Found {len(all_replies) + len(all_comments)} total for aweme_id {aweme_id}.")
+    
     all_replies = process_replies(all_replies, all_comments_)
-    save(all_replies, "replies.csv")
+    replies_filename = f"data/{aweme_id}_replies.csv"
+    save(all_replies, replies_filename)
 
+
+async def main():
+    tasks = [process_aweme_id(aweme_id) for aweme_id in aweme_ids]
+    
+    await asyncio.gather(*tasks)
 
 # 运行 main 函数
 if __name__ == "__main__":
