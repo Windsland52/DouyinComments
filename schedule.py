bfa9670de63b8917413a5fc93e4b7ae9807586cb
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-09-11 01:01:06 +0800

    perf: 为定时任务添加容错

--- schedule.py
+++ schedule.py
@@ -14,17 +14,17 @@ def scheduled_task():
 # 创建调度器
 scheduler = BlockingScheduler()
 
-# 每天早上7:30执行任务
-scheduler.add_job(scheduled_task, 'cron', hour=7, minute=30)
+# 每天早上7:30执行任务，并设置容错时间为1小时
+scheduler.add_job(scheduled_task, 'cron', hour=7, minute=30, misfire_grace_time=3600)
 
-# 每天中午12:00执行任务
-scheduler.add_job(scheduled_task, 'cron', hour=11, minute=30)
+# 每天中午11:30执行任务
+scheduler.add_job(scheduled_task, 'cron', hour=11, minute=30, misfire_grace_time=3600)
 
-# 每天下午6:00执行任务
-scheduler.add_job(scheduled_task, 'cron', hour=17, minute=30)
+# 每天下午5:30执行任务
+scheduler.add_job(scheduled_task, 'cron', hour=17, minute=30, misfire_grace_time=3600)
 
-# 每天晚上8:00执行任务
-scheduler.add_job(scheduled_task, 'cron', hour=19, minute=30)
+# 每天晚上7:30执行任务
+scheduler.add_job(scheduled_task, 'cron', hour=19, minute=30, misfire_grace_time=3600)
 
 # 启动调度器
 try:
